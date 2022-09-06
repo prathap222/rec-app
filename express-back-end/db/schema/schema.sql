a5e84cf5396d48197ba45c51f7673c5e8d5a512e
Prathap <prathap.sims@gmail.com>
2022-09-05 23:14:56 -0700

    update db schema

--- express-back-end/db/schema/schema.sql
+++ express-back-end/db/schema/schema.sql
@@ -1,5 +1,5 @@
 DROP TABLE IF EXISTS itineraries CASCADE;
-DROP TABLE IF EXISTS schedules CASCADE;
+DROP TABLE IF EXISTS planned_activities CASCADE;
 DROP TABLE IF EXISTS trips CASCADE;
 DROP TABLE IF EXISTS activities CASCADE;
 DROP TABLE IF EXISTS destinations CASCADE;
@@ -7,26 +7,26 @@ DROP TABLE IF EXISTS users CASCADE;
 
 
 CREATE TABLE destinations (
-  destination_id SERIAL PRIMARY KEY NOT NULL,
+  id SERIAL PRIMARY KEY NOT NULL,
   city VARCHAR(55) NOT NULL,
   country VARCHAR(55) NOT NULL,
   image_url TEXT
 );
 
 CREATE TABLE activities (
-  activity_id SERIAL PRIMARY KEY NOT NULL,
+  id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(55) NOT NULL,
   price_cents INTEGER NOT NULL,
   operation_time VARCHAR(55) NOT NULL,
-  latitude DECIMAL(10,8) NOT NULL,
-  longitude DECIMAL(11,8) NOT NULL,
+  latitude DECIMAL(10,10) NOT NULL,
+  longitude DECIMAL(11,10) NOT NULL,
   image_url TEXT,
   activity_type VARCHAR(55) NOT NULL,
-  destination_id INTEGER REFERENCES destinations(destination_id) ON DELETE CASCADE
+  destination_id INTEGER REFERENCES destinations(id) ON DELETE CASCADE
 );
 
 CREATE TABLE users (
-  user_id SERIAL PRIMARY KEY NOT NULL,
+  id SERIAL PRIMARY KEY NOT NULL,
   first_name VARCHAR(55) NOT NULL,
   last_name VARCHAR(55) NOT NULL,
   email VARCHAR(55) NOT NULL,
@@ -35,22 +35,22 @@ CREATE TABLE users (
 
 CREATE TABLE trips (
   trip_id uuid PRIMARY KEY NOT NULL,
-  user_id INTEGER REFERENCES users(user_id) ON DELETE CASCADE,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
 
   trip_name VARCHAR(255) NOT NULL,
   trip_budget INTEGER,
   total_cost INTEGER NOT NULL
 );
 
-CREATE TABLE schedules (
-  schedule_id uuid PRIMARY KEY NOT NULL,
+CREATE TABLE planned_activities (
+  id uuid PRIMARY KEY NOT NULL,
   trip_id uuid REFERENCES trips(trip_id) ON DELETE CASCADE,
 
-  schedule_name VARCHAR(255) NOT NULL
+  activity_name VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE itineraries (
-  itinerary_id uuid PRIMARY KEY NOT NULL,
-  schedule_id uuid REFERENCES schedules(schedule_id) ON DELETE CASCADE,
-  activity_id INTEGER REFERENCES activities(activity_id) ON DELETE CASCADE
+  id uuid PRIMARY KEY NOT NULL,
+  planned_activity_id uuid REFERENCES planned_activities(id) ON DELETE CASCADE,
+  activity_id INTEGER REFERENCES activities(id) ON DELETE CASCADE
 );
