c1dc95a5291590825038856cc608c7df426ce419
Prathap <prathap.sims@gmail.com>
2022-09-12 15:55:42 -0700

    package json update and front-end route updates

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,38 +1,61 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import './App.css';
 
-class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      message: 'Welcome to Recreation App!!'
-    }
-  }
+import React, {useState} from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
-  fetchData = () => {
-    axios.get('/api/data') // You can simply make your requests to "/api/whatever you want"
-    .then((response) => {
-      // handle success
-      console.log(response.data) // The entire response from the Rails API
+//components
+import Home from "./screens/home/components/Home"
+import Main from './screens/main/components/Main'
+import Trips from './screens/trips/components/Trips'
+import Login from './screens/login/components/Login'
+import Itinerary from './screens/itinerary/components/Itinerary'
 
-      console.log(response.data.message) // Just the message
-      this.setState({
-        message: response.data.message
-      });
-    })
-  }
 
-  render() {
-    return (
-      <div className="App">
-        <h1>{ this.state.message }</h1>
-        <button onClick={this.fetchData} >
-          Fetch Data
-        </button>
-      </div>
-    );
-  }
+export default function App () {
+
+  let cookieAsObject = Object.fromEntries(
+    document.cookie.split('; ')
+      .map(x => x.split('='))
+      .map(([k, v]) => [k, decodeURIComponent(v)])
+  );
+  let userObject = JSON.parse(cookieAsObject.user || 'null');
+
+  const [user, setUser] = useState(userObject);
+  const [background, setBackGround] = useState("https://dbdzm869oupei.cloudfront.net/img/quadres/preview/31465.png")
+
+  const homeprops = {}
+  homeprops.background = background
+  homeprops.setBackGround = setBackGround
+  homeprops.user = user
+  homeprops.setUser = setUser
+
+  const userprops = {}
+  userprops.user = user
+  userprops.setUser = setUser
+
+
+  return (
+    <Router>
+      <Routes>
+        {console.log(`App.js: background: ${background}`)}
+        {console.log(`App.js: setBackGround: ${setBackGround}`)}
+
+            {/* <Route path="/" element={<Home {...user} {...setUser} {...background} {...setBackGround} />} /> */}
+            {/* <Route path="/login" element={<Login {...user} {...setUser} />} /> */}
+            {/* <Route path="/main/:city/:budget" element={<Main {...user} {...setUser}  />} /> */}
+            {/* <Route path="/edit/:city/:budget/:tripName/:tripId" element={<Main {...user} {...setUser} />} /> */}
+            {/* <Route path="/itinerary/:itineraryId/:country/:city" element={<Itinerary {...user} {...setUser} />} /> */}
+            {/* <Route path="/trips/:userName" element={<Trips {...user} {...setUser} />} /> */}
+
+
+            <Route path="/main/:city/:budget" element={<Main {...userprops}  />} />
+            <Route path="/edit/:city/:budget/:tripName/:tripId" element={<Main {...userprops} />} />
+            <Route path="/itinerary/:itineraryId/:country/:city" element={<Itinerary {...userprops} />} />
+            <Route path="/trips/:userName" element={<Trips {...userprops} />} />
+            <Route path="/login" element={<Login {...userprops} />} />
+            <Route path="/" element={<Home {...homeprops} />} />
+
+      </Routes>
+    </Router>
+
+  );
 }
-
-export default App;
